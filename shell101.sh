e20b7399a6ad8f4235c84c512eebcfc4ebc82765
Santhosh Kumar <sant527@users.noreply.github.com>
2022-09-25 13:57:12 +0530

    Update shell101.sh

--- shell101.sh
+++ shell101.sh
@@ -24,9 +24,9 @@ today=`date +"%Y-%m-%dT%H__%M__%S"`
 
 #format='worstvideo[height>=1000]+worstaudio/bestvideo+worstaudio'
 
-#format='bestvideo+worstaudio'
+format='bestvideo+worstaudio'
 
-format='worstvideo[height>=1000]+bestaudio/best'
+#format='worstvideo[height>=1000]+bestaudio/best'
 
 #format='worstvideo[height>=1000]'
 
@@ -54,18 +54,69 @@ datet=`date +"%Y-%m-%dT%H__%M__%S"`
 # fi
 
 
-#yt-dlp -F "${url}"
+# yt-dlp -F "${url}"
 
-#docker run --rm \
-#--network="avpn" \
-#--env="_JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=lcd'" \
-#--env="DISPLAY=${DISPLAY}" \
-#--volume="/home/administrator/.public_html/youtube_cookies:/home/simha/youtube_cookies" \
-#--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-#--volume="/home/administrator/tmp:/home/simha/tmp:rw" \
-#archlinux_custom:base-devel-20220320.0.50753 /bin/sh -c "curl http://www.myip.ch; cd tmp;yt-dlp --continue --cookies /home/simha/youtube_cookies/youtube.com_cookies.txt --restrict-filenames --no-part --no-mtime --verbose -f '${format}' -o ${today}-%\(title\)s-%\(id\)s.%\(ext\)s ${url}"
+#yt-dlp --continue --cookies /home/administrator/.public_html/youtube_cookies/youtube.com_cookies.txt --restrict-filenames --no-part --no-mtime --verbose -f "${format}" -o "${today}-%(title)s-%(id)s.%(ext)s" "${url}"
 
-yt-dlp --continue --cookies /home/administrator/.public_html/youtube_cookies/youtube.com_cookies.txt --restrict-filenames --no-part --no-mtime --verbose -f "${format}" -o "${today}-%(title)s-%(id)s.%(ext)s" "${url}"
+
+###############################
+# USING DOCKER FILE WITH DIFFERENT NETWORK
+##########################
+
+# for different network to work run docker_routing.sh file
+
+
+dockerfile=$(mktemp)
+trap "rm $dockerfile" EXIT
+# we are escaping $ and \ with \$ and \\
+cat << EOF > $dockerfile
+FROM archlinux:base-devel-20220320.0.50753
+RUN echo "Server = https://archive.archlinux.org/repos/2022/03/25/\\\$repo/os/\\\$arch" > /etc/pacman.d/mirrorlist
+RUN yes | pacman -Syy
+RUN yes | pacman -Syu archlinux-keyring
+# Create user and sudo him
+# check the below step what to replace with what
+RUN sed -i 's@users:x:984:@users:x:1000:@g' /etc/group
+ARG user=simha
+RUN useradd --system -u 1000 -g 1000 -m \$user \\
+  && echo "\$user ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/\$user
+RUN chown simha:users /home/simha
+USER \$user
+WORKDIR /home/\$user
+RUN yes |sudo pacman -S git --noconfirm
+# Install yay
+RUN git clone https://aur.archlinux.org/yay.git \\
+  && cd yay \\
+  && makepkg -sri --needed --noconfirm \\
+  && cd \\
+  # Clean up
+  && rm -rf .cache yay
+# install the required packages
+RUN sudo pacman -S wget --needed --noconfirm
+RUN sudo pacman -S curl --needed --noconfirm
+RUN sudo wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /usr/local/bin/yt-dlp
+RUN sudo chmod a+rx /usr/local/bin/yt-dlp  # Make executable
+RUN sudo pacman -S ffmpeg --needed --noconfirm
+RUN sudo pacman -S python --needed --noconfirm
+EOF
+
+docker build -t arch_with_yay_with_yt_dlp - < $dockerfile
+
+
+if [ -z $(docker network ls --filter name=^avpn$ --format="{{ .Name }}") ] ; then 
+	docker network create --subnet="172.57.0.0/16" -d bridge -o com.docker.network.bridge.name=docker_vpn avpn
+fi
+
+
+docker run --rm \
+--network="avpn" \
+--env="_JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=lcd'" \
+--env="DISPLAY=${DISPLAY}" \
+--volume="/home/administrator/.public_html/youtube_cookies:/home/simha/youtube_cookies" \
+--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+--volume="/home/administrator/tmp:/home/simha/tmp:rw" \
+arch_with_yay_with_yt_dlp /bin/sh -c "curl http://www.myip.ch; cd tmp;yt-dlp -F ${url}; yt-dlp --continue --cookies /home/simha/youtube_cookies/youtube.com_cookies.txt --restrict-filenames --no-part --no-mtime --verbose -f '${format}' -o ${today}-%\(title\)s-%\(id\)s.%\(ext\)s ${url}"
+
 
 #echo "${url}"
 
